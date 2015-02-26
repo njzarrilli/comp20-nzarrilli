3e3e07fd8da04f8d24f2a7449157c7a547946dec
Naomi Zarrilli <njzarrilli@gmail.com>
2015-02-26 14:33:03 -0500

    added ms pac man and header comments

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,3 +1,9 @@
+/*
+ * Naomi Zarrilli
+ * Lab 5
+ * 2/26/15
+ */
+
 function init() {
     var canvas = document.getElementById('game_canvas');
     var context = canvas.getContext('2d');
@@ -6,5 +12,6 @@ function init() {
 
     image.addEventListener("load", function() {
         context.drawImage(image, 322, 0, 465, 140, 0, 0, 465, 140); //draw board
+        context.drawImage(image, 83, 22, 16, 16, 6, 8, 16, 16); //draw ms pacman
     }, false);
 }
