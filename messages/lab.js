0559a3ed40f5d79b38ef0404e35b4051a6e97f01
Naomi Zarrilli <njzarrilli@gmail.com>
2015-03-09 13:01:15 -0400

    added readme and removed unnecessary comments

--- messages/lab.js
+++ messages/lab.js
@@ -1,19 +1,10 @@
 /*Naomi Zarrilli*/
 
 function parse(){
-
-    // 1: create instance of object
     request = new XMLHttpRequest();
-
-    // 3: set up way to managa response --to a function
     request.onreadystatechange = parseData;
-
-    // 2: create/open HTTP request
     request.open("GET", "data.json", true);
-
-    // 4: execute request
     request.send();
-
 }
 
 
